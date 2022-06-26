bc891eb5ab2fedd55aff219e5abc13b00d10fd01
Yash Jagtap <jagtapyash2512@gmail.com>
2022-06-26 10:18:58 +0530

    connectWallet working

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@ import styled from 'styled-components'
 import { useWeb3 } from '@3rdweb/hooks'
 
 export default function Home() {
+  const { address, connectWallet } = useWeb3();
   return (
-    <div></div>
+    <div>
+      <button onClick={() => connectWallet('injected')}>Connect Wallet</button>
+    </div>
   )
 }
