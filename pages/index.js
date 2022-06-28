b346f239c53e8a5d495109295cde6e5e800f4736
Yash Jagtap <jagtapyash2512@gmail.com>
2022-06-28 21:45:11 +0530

    Wrapper

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,18 @@ import { useWeb3 } from '@3rdweb/hooks'
 export default function Home() {
   const { address, connectWallet } = useWeb3();
   return (
-    <div>
+    <Wrapper>
       <button onClick={() => connectWallet('injected')}>Connect Wallet</button>
-    </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  max-width: 100vw;
+  background-color: #0a0b0d;
+  color: white;
+  display: grid;
+  place-items: center;
+`
