4f83839ee4f766c9335f2e8a69647b829d51bf36
Yash Jagtap <jagtapyash2512@gmail.com>
2022-06-29 22:54:02 +0530

    Button

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,9 @@ export default function Home() {
   return (
     <Wrapper>
       <WalletConnect>
-      <button onClick={() => connectWallet('injected')}>
+      <Button onClick={() => connectWallet('injected')}>
         Connect Wallet
-      </button>
+      </Button>
       </WalletConnect>
     </Wrapper>
   )
@@ -28,4 +28,17 @@ const WalletConnect = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items-center;
+`
+const Button = styled.div`
+  /* flex: 0; */
+  border: 1px solid #282b2f;
+  padding: 0.8rem;
+  font-size: 1.3rem;
+  font-weight: 500;
+  border-radius: 0.4rem;
+  background-color: #3773f5;
+  color: #000;
+  &:hover {
+    cursor: pointer;
+  }
 `
