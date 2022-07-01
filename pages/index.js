7d8d3dfe8376be5b60c5a9585ae320c0f109ff49
Yash Jagtap <jagtapyash2512@gmail.com>
2022-07-01 16:55:01 +0530

    Details

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,13 @@ export default function Home() {
   return (
     <Wrapper>
       <WalletConnect>
-      <Button onClick={() => connectWallet('injected')}>
-        Connect Wallet
-      </Button>
+        <Button onClick={() => connectWallet('injected')}>
+          Connect Wallet
+        </Button>
+        <Details>
+          You need Chrome to be
+          <br /> able to run this app.
+        </Details>
       </WalletConnect>
     </Wrapper>
   )
@@ -41,4 +45,11 @@ const Button = styled.div`
   &:hover {
     cursor: pointer;
   }
+`
+const Details = styled.div`
+  font-size: 1.2rem;
+  text-align: center;
+  margin-top: 1rem;
+  font-weight: 500;
+  color: #282b2f;
 `
