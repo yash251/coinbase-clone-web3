a01732e660b998a9f416e7f6d59e0b938d7fb2c8
Yash Jagtap <jagtapyash2512@gmail.com>
2022-07-05 17:41:27 +0530

    render dashboard address

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Dashboard = () => {
+const Dashboard = ({address}) => {
   return (
-    <div>Dashboard</div>
+    <div>{address}</div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { useWeb3 } from '@3rdweb/hooks'
+import Dashboard from './Dashboard';
 
 export default function Home() {
   const { address, connectWallet } = useWeb3();
@@ -7,7 +8,7 @@ export default function Home() {
   return (
     <Wrapper>
       {address ? (
-        <h2>{address}</h2>
+        <Dashboard address={address} />
       ) : (
         <WalletConnect>
           <Button onClick={() => connectWallet('injected')}>
