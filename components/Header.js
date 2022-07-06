a14f6da50687d2d36be7124fd66f54a2efe60d68
Yash Jagtap <jagtapyash2512@gmail.com>
2022-07-06 21:45:31 +0530

    header styling

--- components/Header.js
+++ components/Header.js
@@ -7,3 +7,31 @@ const Header = () => {
 }
 
 export default Header
+
+const Wrapper = styled.div`
+  width: calc(100% - 3rem);
+  padding: 1rem 1.5rem;
+  border-bottom: 1px solid #282b2f;
+  display: flex;
+  align-items: center;
+`
+const Title = styled.div`
+  font-size: 2rem;
+  font-weight: 600;
+  flex: 1;
+`
+const ButtonsContainer = styled.div`
+  display: flex;
+`
+const Button = styled.div`
+  border: 1px solid #282b2f;
+  padding: 0.8rem;
+  font-size: 1.3rem;
+  font-weight: 500;
+  border-radius: 0.4rem;
+  margin-right: 1rem;
+  
+  &:hover {
+    cursor: pointer;
+  }
+`
