ad52f683b542f2e8d2cb9eb7354b097b4dfe29b2
Yash Jagtap <jagtapyash2512@gmail.com>
2022-07-08 19:08:44 +0530

    btn flex

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,12 @@ import styled from 'styled-components'
 const Header = () => {
   return (
     <Wrapper>
-        <Button style={{ backgroundColor: '#3773f5', color: '#000' }}>
-            Buy / Sell
-        </Button>
-        <Button>Send / Receive</Button>
+        <ButtonsContainer>
+            <Button style={{ backgroundColor: '#3773f5', color: '#000' }}>
+                Buy / Sell
+            </Button>
+            <Button>Send / Receive</Button>
+        </ButtonsContainer>
     </Wrapper>
   )
 }
