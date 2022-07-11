0628e4acc1c055c9a66b673c8a250a1af719ef34
Yash Jagtap <jagtapyash2512@gmail.com>
2022-07-11 21:15:30 +0530

    header moved to top

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import Header from '../components/Header'
+import styled from 'styled-components'
 
 const Dashboard = ({address}) => {
   return (
-    <Header />
+    <Wrapper>
+      <MainContainer>
+        <Header />
+      </MainContainer>
+    </Wrapper>
   )
 }
 
 export default Dashboard
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: #0a0b0d;
+  color: white;
+`
+
+const MainContainer = styled.div`
+  flex: 1;
+`
