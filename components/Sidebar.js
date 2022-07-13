469870a5e72cfa183dba5672c42e9704ed7e35fb
Yash Jagtap <jagtapyash2512@gmail.com>
2022-07-13 17:18:03 +0530

    sidebar styling

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Sidebar = () => {
   return (
@@ -7,3 +8,46 @@ const Sidebar = () => {
 }
 
 export default Sidebar
+
+const Wrapper = styled.div`
+  height: calc(100vh);
+  border-right: 1px solid #282b2f;
+  width: calc(22rem - 16px - 16px);
+  padding: 0 1rem;
+`
+const LogoContainer = styled.div`
+  margin: 1.5rem 0;
+`
+const Logo = styled.div`
+  width: 44%;
+  object-fit: contain;
+  margin-left: 1.5rem;
+`
+const NavItemsContainer = styled.div`
+  margin-top: 3rem;
+
+  &:hover {
+    cursor: pointer;
+  }
+`
+const NavItem = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 1.3rem;
+  font-weight: 500;
+  border-radius: 0.5rem;
+  margin-bottom: 1.5rem;
+  height: 4rem;
+
+  &:hover {
+    background-color: #141519;
+  }
+`
+const NavIcon = styled.div`
+  background-color: #141519;
+  padding: 0.7rem;
+  border-radius: 50%;
+  margin: 0 1rem;
+  display: grid;
+  place-items: center;
+`
