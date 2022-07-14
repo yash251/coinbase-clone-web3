01b15e5c5f409f427bf5c385338799d7a6004de7
Yash Jagtap <jagtapyash2512@gmail.com>
2022-07-14 20:41:29 +0530

    additional sidebar styling using useState

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import Image from 'next/image'
 import CoinbaseLogo from '../assets/cb-logo.png'
 import { navItems } from '../static/navItems'
 
 const Sidebar = () => {
+    
+    const [activeIcon, setActiveIcon] = useState(navItems[0].title)
+    
   return (
     <Wrapper>
         <LogoContainer>
@@ -14,8 +17,10 @@ const Sidebar = () => {
         </LogoContainer>
         <NavItemsContainer>
             {navItems.map((item, index) => (
-                <NavItem key={index}>
-                    <NavIcon>{item.icon}</NavIcon>
+                <NavItem key={index} onClick={() => setActiveIcon(item.title)}>
+                    <NavIcon style={{ color: item.title === activeIcon && '#3773f5' }}>
+                        {item.icon}
+                    </NavIcon>
                     <NavTitle>{item.title}</NavTitle>
                 </NavItem>
             ))}
