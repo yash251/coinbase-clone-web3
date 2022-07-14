530b76ccb168e0ecbc4d691bc8e66300c6ae6124
Yash Jagtap <jagtapyash2512@gmail.com>
2022-07-14 20:24:01 +0530

    wallet btn styling

--- components/Header.js
+++ components/Header.js
@@ -44,3 +44,24 @@ const Button = styled.div`
     cursor: pointer;
   }
 `
+const WalletLink = styled.div`
+  font-size: 0.8rem;
+  border: 1px solid #282b2f;
+  border-radius: 50rem;
+  font-size: 1.2rem;
+  margin-right: 1rem;
+  padding: 0 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+`
+const WalletLinkTitle = styled.div`
+  font-size: 1.1rem;
+  margin-bottom: 0.3rem;
+  color: #27ad75;
+  font-weight: 600;
+`
+const WalletAddress = styled.div`
+  font-size: 0.8rem;
+`
