365e9e9960f70210baf9f3c3e02131a657cd4270
Yash Jagtap <jagtapyash2512@gmail.com>
2022-08-01 19:46:20 +0530

    portfolio-> dashboard

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,10 +1,44 @@
-import React from 'react'
 import Header from '../components/Header'
 import styled from 'styled-components'
 import Main from '../components/Main'
 import Sidebar from '../components/Sidebar'
+import { ThirdwebSDK } from '@3rdweb/sdk'
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
 
 const Dashboard = ({ address }) => {
+  const [twTokens, setTwTokens] = useState([])
+  const [sanityTokens, setSanityTokens] = useState([])
+
+  const sdk = new ThirdwebSDK(
+    new ethers.Wallet(
+        process.env.NEXT_PUBLIC_METAMASK_KEY,
+        ethers.getDefaultProvider(
+            'https://rinkeby.infura.io/v3/47465af1e4924253935d625c19b1b514'
+        )
+    )
+)
+
+  useEffect(() => {
+    const getCoins = async () => {
+      try {
+        const coins = await fetch(
+          "https://3wdlx5s1.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D'coins'%5D%20%7B%0A%20%20name%2C%0A%20%20usdPrice%2C%0A%20%20contractAddress%2C%0A%20%20symbol%2C%0A%20%20logo%2C%0A%7D",
+        )
+        const tempSanityTokens = await coins.json()
+
+        setSanityTokens(tempSanityTokens.result)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    return getCoins()
+  }, [])
+
+  console.log('Sanity ', sanityTokens);
+  console.log('Thirdweb', thirdWebTokens);
+
   return (
     <Wrapper>
       <Sidebar />
